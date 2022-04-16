c2b09661c2d46105573c5b276577b8b22e4b6b0a
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-04-16 13:30:12 +0100

    second commit

--- src/Main/Application.java
+++ src/Main/Application.java
@@ -1,7 +1,9 @@
 package Main;
 
+import ProductMgt.Product;
 import customerMgt.Customer;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +11,7 @@ public class Application {
 
     public static void main(String[] args){
         //créer un Customer ayant comme firstName "Mohamed" et comme lastName "Ben Salah"
-        Customer c1=new Customer();//l'instantiation (la reservation d'un espace mémoire)
+     /*   Customer c1=new Customer();//l'instantiation (la reservation d'un espace mémoire)
         c1.setFirstName("Mohamed");
         c1.setLastName("Ben Salah");
         System.out.println(c1);
@@ -18,7 +20,7 @@ public class Application {
         c2.setFirstName("Ahmed");
         c2.setLastName("Ben mohamed");
         System.out.println(c2);
-        /*List<Customer> customers=new ArrayList<>();
+        List<Customer> customers=new ArrayList<>();
         customers.add(c1);
         customers.add(c2);
         System.out.println("Before remove : ");
@@ -31,8 +33,12 @@ public class Application {
         System.out.println("After remove : ");
         System.out.println(customers);
 */
-
-
+        //test of lombok library use
+        Product p1=new Product();
+        p1.setLabel("Product 1");
+        p1.setExpiredDate(LocalDate.of(2022,11,12));
+        p1.setFabricationDate(LocalDate.of(2021,11,12));
+        System.out.println(p1);
 
     }
 }
--- src/ProductMgt/Product.java
+++ src/ProductMgt/Product.java
@@ -1,4 +1,34 @@
 package ProductMgt;
 
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter //generates all getters for all attributes
+@Setter //generates all setters
+@AllArgsConstructor //generates an constructor with all attributes
+//@NoArgsConstructor //generates a no args constructor
+@ToString //overrides the method toString using all attributes
+//@EqualsAndHashCode //overrides equals and hashCode methods using all attributes
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)//uses only attributes annotated with Include for comparing
 public class Product {
+    private long id;
+    private static long lastId=0;
+    @EqualsAndHashCode.Include
+    private String label;
+    private BigDecimal price;
+    @EqualsAndHashCode.Include
+    private LocalDate expiredDate;
+    @EqualsAndHashCode.Include
+    private LocalDate fabricationDate;
+    private int stock;
+    private String description;
+    @ToString.Exclude
+    private byte[] photo;
+    public Product()
+    {
+        id=++lastId;
+    }
+
 }
